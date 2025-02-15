c97609495146ff3a5fc4c159f37db7d2997d5f5e
Aidan Olsen <a.olsen2024@icloud.com>
2025-02-15 11:14:51 -0700

    Setup expressjs

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
