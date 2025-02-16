262478a96cd6fb355f723c943d58e9ce59a1da1d
adityadipakpatel <adityadipakpatel@gmail.com>
2025-02-16 16:43:15 -0700

    the userController.js actually works for profile page

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -3,25 +3,23 @@ const ApiError = require("../utils/ApiError"); // Import ApiError for error hand
 const controller = require("../utils/controller"); // Import controller wrapper
 
 const getUserProfile = async (req, res) => {
-    const user = await prisma.user.findUnique({
-      where: { id: req.session.userId },
-      select: {
-        id: true,
-        username: true,
-        name: true,
-        email: true,
-        profilePic: true,
-      },
-    });
-    if (!user) throw ApiError(404, "User not found");
+  const user = await prisma.user.findUnique({
+    where: { id: req.session.userId },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      profilePic: true,
+    },
+  });
+  if (!user) throw new ApiError(404, "User not found");
 
-    res.send({
-      id: user.id,
-      username: user.username,
-      name: user.name || "Not Provided",
-      email: user.email,
-      profilePic: user.profilePic ? `/uploads/${user.profilePic}` : "/uploads/default.png",
-    });
+  res.send({
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    profilePic: user.profilePic ? `/uploads/${user.profilePic}` : "/uploads/default.png",
+  });
 };
 
 module.exports = { getUserProfile: controller(getUserProfile) };
