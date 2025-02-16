1187b0bc79213315955bedf5c9c80bbeec3e209f
Aidan Olsen <a.olsen2024@icloud.com>
2025-02-15 18:18:43 -0700

    Remove console.log

--- backend/src/services/listings.js
+++ backend/src/services/listings.js
@@ -1,12 +1,6 @@
 const db = require("../db");
 
 async function create(userId, listing) {
-  console.log({
-    title: listing.title,
-    description: listing.description,
-    price: listing.price,
-    sellerId: userId,
-  });
   const created = await db.listing.create({
     data: {
       title: listing.title,
