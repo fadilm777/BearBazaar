57bb3a3a4bd5903ff92cab65bc76bb61a57d07dc
adityadipakpatel <adityadipakpatel@gmail.com>
2025-02-15 18:52:29 -0700

    fixed more merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useState } from "react";
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 import "./App.css";
 import bearBazaar from "./assets/bearbazaar.svg";
