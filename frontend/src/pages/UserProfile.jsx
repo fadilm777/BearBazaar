3706a4c2145bbb56fbdf9ae86c6647afaa3d9638
fadilm777 <fadil.m777@gmail.com>
2025-02-16 17:43:04 -0700

    oopsies

---
+++ frontend/src/pages/UserProfile.jsx
@@ -0,0 +1,12 @@
+import AppProfile from '@/components/appProfile'
+
+const UserProfile = () => {
+  return (
+    <div className='w-full justify-center'>
+      <div className='h-32' />
+      <AppProfile />
+    </div>
+  )
+}
+
+export default UserProfile
